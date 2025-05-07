95f93e2835e5fcc88b9007d754376dcd11915276
pjuyong <imjuyongp@naver.com>
2025-05-07 20:40:22 +0900

    🔧 Settings: SpringStudyApplication 위치 이동

--- src/main/java/com/likelion/sbstudy/SpringStudyApplication.java
+++ src/main/java/com/likelion/sbstudy/SpringStudyApplication.java
@@ -2,7 +2,9 @@ package hello.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA가 알아서 엔티티가 생성, 수정될 때의 시간을 자동으로 넣어줌
 @SpringBootApplication
 public class SpringStudyApplication {
 
